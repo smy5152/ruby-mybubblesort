34622e5e15e18e3163646f69d14cbd66e4678e07
smy5152 <shabbiryamani@gmail.com>
2021-02-10 01:30:41 +0200

    Rubocop: Linters Fixed

--- mybubblesort-yield.rb
+++ mybubblesort-yield.rb
@@ -1,29 +1,29 @@
 # method: Sorting for received list of strings
 # parameter: an array of strings
 # Type: Bubble sort with a double for loop and a temp swap variable
-# Did not use the Ruby feature of variable swap to leave it as basic as possible 
+# Did not use the Ruby feature of variable swap to leave it as basic as possible
 
 def my_bubble_sort(list_to_sort)
   swap = ""
   for i in 0...(list_to_sort.length)
-    for j in 0...(list_to_sort.length-1)
-      yield_result = yield(list_to_sort[j],list_to_sort[j+1])  
+    for j in 0...(list_to_sort.length - 1)
+      yield_result = yield(list_to_sort[j], list_to_sort[j + 1])  
       if yield_result.positive?
-        swap=list_to_sort[j]
-        list_to_sort[j]=list_to_sort[j+1]
-        list_to_sort[j+1]=swap
+        swap = list_to_sort[j]
+        list_to_sort[j] = list_to_sort[j + 1]
+        list_to_sort[j + 1] = swap
       end
-    end 
+    end
   end
-  #implicit return
+  # implicit return
   list_to_sort
 end
 
 # INPUT: List of Strings
-list = ["hi","likes","hey", "adam", "wassabi"]
+list = ["hi", "likes", "hey", "adam", "wassabi"]
 # CALL: Sort Function
 # YIELD: Function block defined to compare and decide whether swap is needed
-my_bubble_sort(list) do |left,right|
+my_bubble_sort(list) do |left, right|
   left.length - right.length
 end
 # Print: Sorted array
--- mybubblesort.rb
+++ mybubblesort.rb
@@ -1,20 +1,20 @@
 # method: Sorting for received list of numbers
 # parameter: an array of numbers
 # Type: Bubble sort with a double for loop and a temp swap variable
-# Did not use the Ruby feature of variable swap to leave it as basic as possible 
+# Did not use the Ruby feature of variable swap to leave it as basic as possible
 
 def my_bubble_sort(list_to_sort)
   swap = 0
   for i in 0...(list_to_sort.length)
-    for j in 0...(list_to_sort.length-1)
-      if list_to_sort[j]>list_to_sort[j+1]
-        swap=list_to_sort[j]
-        list_to_sort[j]=list_to_sort[j+1]
-        list_to_sort[j+1]=swap
+    for j in 0...(list_to_sort.length - 1)
+      if list_to_sort[j] > list_to_sort[j + 1]
+        swap = list_to_sort[j]
+        list_to_sort[j] = list_to_sort[j + 1]
+        list_to_sort[j + 1] = swap
       end
-    end 
+    end
   end
-  #implicit return
+  # implicit return
   list_to_sort
 end
 
