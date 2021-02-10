96627a9c00de584ce785b87762885d1a14d96edd
Diego Solis <disc3110@gmail.com>
2021-02-10 08:59:01 -0600

    Update mybubblesort.rb

--- mybubblesort.rb
+++ mybubblesort.rb
@@ -8,9 +8,9 @@ def my_bubble_sort(list_to_sort)
     (0...list_to_sort.length - 1).each do |j|
       next if list_to_sort[j] < list_to_sort[j + 1]
 
-        swap = list_to_sort[j]
-        list_to_sort[j] = list_to_sort[j + 1]
-        list_to_sort[j + 1] = swap
+      swap = list_to_sort[j]
+      list_to_sort[j] = list_to_sort[j + 1]
+      list_to_sort[j + 1] = swap
     end
   end
   # implicit return
