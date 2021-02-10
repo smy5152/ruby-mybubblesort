34c83a91a4b61213720df476ad6b372afff78ba6
Diego Solis <disc3110@gmail.com>
2021-02-10 08:50:35 -0600

    fix linter errors

--- bubbles.rb
+++ bubbles.rb
@@ -1,36 +1,36 @@
 def bubble_sort(bubbles)
   ready = false
-  while ready==false do
+  while ready == false
     ready = true
-    (0...bubbles.length-1).each do |i|
-      if bubbles[i] > bubbles[i+1]
-        bubbles[i], bubbles[i+1] = bubbles[i+1], bubbles[i]
+    (0...bubbles.length - 1).each do |i|
+      if bubbles[i] > bubbles[i + 1]
+        bubbles[i], bubbles[i + 1] = bubbles[i + 1], bubbles[i]
         ready = false
       end
     end
   end
 end
-example_arr_1 = [19,3, 27, 34, 9, 1, 10, 100, 2]
-bubble_sort(example_arr_1)
-p example_arr_1
+examplearr1 = [19, 3, 27, 34, 9, 1, 10, 100, 2]
+bubble_sort(examplearr1)
+p examplearr1
 
-#Now the Microverse variant
+# Now the Microverse variant
 def bubble_sort_by(bubbles)
   ready = false
-  while ready==false do
+  while ready==false
     ready = true
-    (0...bubbles.length-1).each do |i|
-      comparation = yield bubbles[i], bubbles[i+1]
+    (0...bubbles.length - 1).each do |i|
+      comparation = yield bubbles[i], bubbles[i + 1]
       if comparation.positive?
-        bubbles[i], bubbles[i+1] = bubbles[i+1], bubbles[i]
+        bubbles[i], bubbles[i + 1] = bubbles[i + 1], bubbles[i]
         ready = false
       end
     end
   end
 end
 
-example_arr_2 = ["hi","hello","hey"]
-bubble_sort_by(example_arr_2) do |left,right|
+examplearr2 = w%[hi hello hey]
+bubble_sort_by(examplearr2) do |left, right|
   left.length - right.length
 end
-p example_arr_2
+p examplearr2
