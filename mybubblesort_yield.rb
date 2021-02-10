92da0ec303cc2fd0d45baad195d455c03eadd45c
Diego Solis <disc3110@gmail.com>
2021-02-10 08:20:16 -0600

    Update mybubblesort_yield.rb

--- mybubblesort_yield.rb
+++ mybubblesort_yield.rb
@@ -7,7 +7,7 @@ def my_bubble_sort(list_to_sort)
   for i in 0...(list_to_sort.length)
     for j in 0...(list_to_sort.length - 1)
       yield_result = yield(list_to_sort[j], list_to_sort[j + 1])
-      next if yield_result.positive?
+        next if yield_result.positive?
         swap = list_to_sort[j]
         list_to_sort[j] = list_to_sort[j + 1]
         list_to_sort[j + 1] = swap
@@ -18,7 +18,7 @@ def my_bubble_sort(list_to_sort)
 end
 
 # INPUT: List of Strings
-list = %w@'hi', 'likes', 'hey', 'adam', 'wassabi'@
+list = %w[hi likes hey adam wassabi]
 # CALL: Sort Function
 # YIELD: Function block defined to compare and decide whether swap is needed
 my_bubble_sort(list) do |left, right|
